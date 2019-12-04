64a8ddba4746a6cb7de31a4f0468fc599121213a
jcgalela <galelajohnchristian@gmail.com>
2019-12-04 11:53:48 +0800

    empsys library

---
+++ EmpSysLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpSysLibrary
+{
+    public class Class1
+    {
+    }
+}
